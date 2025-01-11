9c3039b1a3f67430719c15064c1b5d49d14432ac
Sammy-art-creator <svmoralesz@gmail.com>
2025-01-11 20:01:57 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -9,3 +9,21 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+let action = ["ate ", "peed ", "crushed ", "broke "];
+let what = ["my homework ", "my phone ", "the car "];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+let randomwho = Math.floor(Math.random() * who.length);
+let randomaction = Math.floor(Math.random() * action.length);
+let randomwhat = Math.floor(Math.random() * what.length);
+let randomwhen = Math.floor(Math.random() * when.length);
+
+document.querySelector("#excuse").innerHTML =
+  who[randomwho] + action[randomaction] + what[randomwhat] + when[randomwhen];
